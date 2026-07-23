0e4b95661942257f40db94feb25e73d9ebbe82d7
James Walker <jmmwiv4@gmail.com>
2026-07-23 16:55:28 -0700

    fix: load astronomy engine in serverless runtime

--- api/lib/morning-brief.js
+++ api/lib/morning-brief.js
@@ -1,9 +1,11 @@
-import {
+import * as AstronomyModule from 'astronomy-engine'
+
+const {
   Body,
   Ecliptic,
   GeoVector,
   MoonPhase,
-} from 'astronomy-engine'
+} = AstronomyModule.default || AstronomyModule
 
 export const LIFE_PROJECTS_DATABASE_ID = 'd54d374d-bf65-4e52-955e-22138623388c'
 export const BODY_SIGNALS_DATABASE_ID = '01a5184fc8c340e0a0545ab4b0881585'
